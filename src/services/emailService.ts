5f5adf3af91411816eae547fb7024b98608152a5
hugobernar <hugolima2134@gmail.com>
2025-05-29 15:00:24 -0300

    Atualiza valor na página

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -8,22 +8,37 @@ import {
   deleteDoc,
   doc,
 } from "firebase/firestore";
+import { EmailAccount } from "../types"; // ajuste o caminho conforme seu projeto
 
 // Referência à coleção 'emails' no Firestore
 const collectionRef = collection(db, "emails");
 
 // Buscar todos os emails
-export async function getAllEmailsFromFirebase() {
+//export async function getAllEmailsFromFirebase() {
+ // const snapshot = await getDocs(collectionRef);
+ // return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+ export async function getAllEmailsFromFirebase(): Promise<EmailAccount[]> {
   const snapshot = await getDocs(collectionRef);
-  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  const data = snapshot.docs.map((doc) => {
+    const d = doc.data();
+    return {
+      id: doc.id,
+      name: d.name,
+      email: d.email,
+      department: d.department,
+      description: d.description,
+    };
+  });
+  return data;
 }
+
 
 // Adicionar um novo email
 export async function addEmailToFirebase(data: {
-  nome: string;
+  name: string;
   email: string;
-  departamento: string;
-  descricao: string;
+  department: string;
+  description: string;
 }) {
   const docRef = await addDoc(collectionRef, data);
   return { id: docRef.id, ...data }; // retorna o novo email já com ID
@@ -31,10 +46,10 @@ export async function addEmailToFirebase(data: {
 
 // Atualizar um email existente
 export async function updateEmailInFirebase(id: string, data: {
-  nome: string;
+  name: string;
   email: string;
-  departamento: string;
-  descricao: string;
+  department: string;
+  description: string;
 }) {
   const ref = doc(db, "emails", id);
   return await updateDoc(ref, data);
